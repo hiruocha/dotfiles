96df99bb324f448ba7b16a49137269cbe435ce6f
hiruocha <hiruocha@gmail.com>
2025-07-16 05:21:54 +0800

    Update

--- omz/dot-oh-my-zsh/custom/env.zsh
+++ omz/dot-oh-my-zsh/custom/env.zsh
@@ -2,4 +2,13 @@
 # $ZSH_CUSTOM/env.zsh
 #
 
+####################
+# mirrors          #
+####################
+
+# repo
 export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo'
+
+# go
+export GO111MODULE=on
+export GOPROXY=https://goproxy.cn
