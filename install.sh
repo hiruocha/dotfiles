26b421b2c4ccf0ec22f6cc55e9809daddd4120d0
hiruocha <hiruocha@gmail.com>
2025-07-19 21:09:20 +0800

    Update

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ user_config() {
         case $answer in
         Y | y)
           echo "导入中……"
-          stow --target=~ "$@" "$CONFIG"
+	  stow --target=/home/$USER "$@" "$CONFIG"
           break
 	  ;;
         N | n)
