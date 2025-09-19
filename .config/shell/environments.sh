f515369e7ae82ac4f86b5383260e27919900614d
hiruocha <hiruocha@gmail.com>
2025-09-19 11:12:03 +0800

    mod(shell/environments.sh): 改进逻辑

--- .config/shell/environments.sh
+++ .config/shell/environments.sh
@@ -1,25 +1,37 @@
 # Copyright © 2025 hiruocha
 
+add_path () {
+  [ -d "$1" ] && \
+  case :"$PATH": in
+    *:"$1":*)
+      ;;
+    *)
+      export PATH="$1:$PATH"
+      ;;
+  esac
+}
+
 # local path
-[ -d "$HOME/.local/bin" ] && export PATH=$HOME/.local/bin:$PATH
+add_path "$HOME/.local/bin"
 
 # editor
 { [ -n "$(command -v nvim)" ] && export EDITOR='nvim'; } || \
 { [ -n "$(command -v vim)" ] && export EDITOR='vim'; } || \
 { [ -n "$(command -v nano)" ] && export EDITOR='nano'; }
+export VISUAL="$EDITOR"
 
 # flutter
-[ -d "$HOME/.local/bin/flutter/bin" ] && export PATH="$HOME/.local/bin/flutter/bin:$PATH"
+add_path "$HOME/.local/bin/flutter/bin"
 export PUB_HOSTED_URL="https://pub.flutter-io.cn"
 export FLUTTER_STORAGE_BASE_URL="https://storage.flutter-io.cn"
 [ -n "$(command -v chromium)" ] && export CHROME_EXECUTABLE='/usr/bin/chromium'
 
 # ccache
-if [ -n "$(command -v ccache)" ]; then
-  [ -d "/usr/lib/ccache/bin" ] && export PATH="/usr/lib/ccache/bin:$PATH"
+[ -n "$(command -v ccache)" ] && {
+  add_path "/usr/lib/ccache/bin"
   export USE_CCACHE=1
   export CCACHE_EXEC="$(command -v ccache)"
-fi
+}
 
 # repo
 export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo'
