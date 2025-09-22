0480f0a87631ae32c4533fd8dfdfc123586ab2ba
hiruocha <hiruocha@gmail.com>
2025-09-22 13:39:04 +0800

    mod(install.sh): 改进安装脚本
    在 bash 运行完退出后不要再生成新的 ~/.bash_history

--- install.sh
+++ install.sh
@@ -5,4 +5,7 @@ stow -t "$HOME" --no-folding -R .
 
 [ -L "$HOME"/.ssh/config ] && { chmod 700 "$HOME"/.ssh && chmod 600 "$HOME"/.ssh/config; }
 
+[ -d "$HOME"/.local/state ] || mkdir -p "$HOME"/.local/state
 [ -f "$HOME"/.bash_history ] && mv "$HOME"/.bash_history "$HOME"/.local/state/bash_history
+
+[ -n "$BASH_VERSION" ] && . "$HOME"/.bashrc
