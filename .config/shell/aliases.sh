cce55da5d39f92d06c592ffd1c6510e28cb39c0e
hiruocha <hiruocha@gmail.com>
2025-09-26 08:40:55 +0800

    mod(shell/aliases.sh): add "l la ll" aliases

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -1,6 +1,10 @@
 # Copyright © 2025 hiruocha
 
-alias ls='ls --color=auto'
+alias ls='ls --color=auto -F'
 alias grep='grep --color=auto'
 alias ip='ip -c'
 #alias ls='eza --icons --git --git-repos'
+
+alias l='ls -lAh'
+alias ll='ls -lh'
+alias la='ls -A'
