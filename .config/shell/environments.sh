efc2d503a7086f2ed486870f0a9491bcc81c2573
hiruocha <hiruocha@gmail.com>
2025-09-26 12:18:53 +0800

    mod(shell/environments.sh): 更改 nodejs 镜像源

--- .config/shell/environments.sh
+++ .config/shell/environments.sh
@@ -46,6 +46,6 @@ export RUSTUP_UPDATE_ROOT='https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup'
 export RUSTUP_DIST_SERVER='https://mirrors.tuna.tsinghua.edu.cn/rustup'
 
 # nodejs
-export NVM_NODEJS_ORG_MIRROR='https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/'
+export NVM_NODEJS_ORG_MIRROR=https://mirrors.ustc.edu.cn/node/
 
 unset -f add_path
