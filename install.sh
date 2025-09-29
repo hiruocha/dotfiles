526082b211b3bb4173f036d103b4c1287821229b
hiruocha <hiruocha@gmail.com>
2025-09-29 11:17:09 +0800

    mod(install.sh): 检测运行路径

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 
 set -e
 
+[ "$(git rev-parse --show-toplevel 2>/dev/null)" = "$(pwd)" ] || \
+{ echo "Please run this script from the Git repository root"; exit 1; }
+
 stow -t "$HOME" --no-folding -R .
 
 [ -L "$HOME"/.ssh/config ] && { chmod 700 "$HOME"/.ssh && chmod 600 "$HOME"/.ssh/config; } || true
