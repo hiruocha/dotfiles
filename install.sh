be6f642d4a11d34e8cdbe2c33bfdffb9daa53ca0
hiruocha <hiruocha@gmail.com>
2025-09-29 11:33:41 +0800

    mod(install.sh): 优化脚本，提升可读性

--- install.sh
+++ install.sh
@@ -3,14 +3,23 @@
 
 set -e
 
-[ "$(git rev-parse --show-toplevel 2>/dev/null)" = "$(pwd)" ] || \
-{ echo "Please run this script from the Git repository root"; exit 1; }
+[ "$(git rev-parse --show-toplevel 2>/dev/null)" = "$(pwd)" ] || {
+  echo "Please run this script from the Git repository root"
+  exit 1
+}
 
 stow -t "$HOME" --no-folding -R .
 
-[ -L "$HOME"/.ssh/config ] && { chmod 700 "$HOME"/.ssh && chmod 600 "$HOME"/.ssh/config; } || true
+[ -L "$HOME"/.ssh/config ] && {
+  chmod 700 "$HOME"/.ssh
+  chmod 600 "$HOME"/.ssh/config
+} || true
 
-[ -d "$HOME"/.local/state/bash ] || mkdir -p "$HOME"/.local/state/bash
-[ -f "$HOME"/.bash_history ] && mv "$HOME"/.bash_history "$HOME"/.local/state/bash/history || true
+[ -d "$HOME"/.local/state/bash ] || \
+mkdir -p "$HOME"/.local/state/bash
+[ -f "$HOME"/.bash_history ] && \
+[ ! -f "$HOME"/.local/state/bash/history ] && \
+mv "$HOME"/.bash_history "$HOME"/.local/state/bash/history || \
+true
 
 echo "If you are running bash, please run: source ~/.bashrc"
