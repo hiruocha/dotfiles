b846eeae201ca5009c55aff95c0e18f4a401a937
hiruocha <hiruocha@gmail.com>
2025-09-29 09:40:23 +0800

    mod(install.sh): 完善安装脚本

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # Copyright © 2025 hiruocha
 
+set -e
+
 stow -t "$HOME" --no-folding -R .
 
-[ -L "$HOME"/.ssh/config ] && { chmod 700 "$HOME"/.ssh && chmod 600 "$HOME"/.ssh/config; }
+[ -L "$HOME"/.ssh/config ] && { chmod 700 "$HOME"/.ssh && chmod 600 "$HOME"/.ssh/config; } || true
 
-[ -d "$HOME"/.local/state ] || mkdir -p "$HOME"/.local/state
-[ -f "$HOME"/.bash_history ] && mv "$HOME"/.bash_history "$HOME"/.local/state/bash_history
+[ -d "$HOME"/.local/state/bash ] || mkdir -p "$HOME"/.local/state/bash
+[ -f "$HOME"/.bash_history ] && mv "$HOME"/.bash_history "$HOME"/.local/state/bash/history || true
 
-[ -n "$BASH_VERSION" ] && . "$HOME"/.bashrc || true
+echo "If you are running bash, please run: source ~/.bashrc"
