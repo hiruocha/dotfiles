620ccb73b826e32cfce0d165a7736d86130d2d78
hiruocha <hiruocha@tuta.io>
2025-11-15 15:25:06 +0800

    add: 初步配置 nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- Copyright © 2025 hiruocha
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+vim.opt.colorcolumn = "120"
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 0
