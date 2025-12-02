506062c0952ef5776ed1356e16498198c8813acd
hiruocha <hiruocha@gmail.com>
2025-12-03 05:23:46 +0800

    mod: 更改 XDG 用户目录为首字母大写

--- .config/shell/profile.sh
+++ .config/shell/profile.sh
@@ -32,14 +32,14 @@ export XDG_DATA_HOME="$HOME"/.local/share
 export XDG_STATE_HOME="$HOME"/.local/state
 
 # xdg userdirs
-export XDG_DESKTOP_DIR="$HOME"/desktop
-export XDG_DOWNLOAD_DIR="$HOME"/downloads
-export XDG_TEMPLATES_DIR="$HOME"/templates
-export XDG_PUBLICSHARE_DIR="$HOME"/public
-export XDG_DOCUMENTS_DIR="$HOME"/documents
-export XDG_MUSIC_DIR="$HOME"/music
-export XDG_PICTURES_DIR="$HOME"/pictures
-export XDG_VIDEOS_DIR="$HOME"/videos
+export XDG_DESKTOP_DIR="$HOME"/Desktop
+export XDG_DOWNLOAD_DIR="$HOME"/Downloads
+export XDG_TEMPLATES_DIR="$HOME"/Templates
+export XDG_PUBLICSHARE_DIR="$HOME"/Public
+export XDG_DOCUMENTS_DIR="$HOME"/Documents
+export XDG_MUSIC_DIR="$HOME"/Music
+export XDG_PICTURES_DIR="$HOME"/Pictures
+export XDG_VIDEOS_DIR="$HOME"/Videos
 
 # fcitx5
 export XMODIFIERS=@im=fcitx
