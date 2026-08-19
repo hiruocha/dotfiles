edc7fe8f9c79c18583a60425ec3a65a728c6666b
willker <wz.willker@gmail.com>
2026-08-20 01:14:29 +0800

    shell(profile): 重命名 profile 为 environment，重新分离 bash_profile 和 environment

--- shell/environment.sh
+++ shell/environment.sh
@@ -71,8 +71,3 @@ path "/usr/local/bin"
 path "$HOME/.local/bin"
 
 unset -f path
-
-if [ -n "$BASH_VERSION" ] && [ -f "$HOME/.bashrc" ]; then
-  # shellcheck source=/dev/null
-  . "$HOME/.bashrc"
-fi
